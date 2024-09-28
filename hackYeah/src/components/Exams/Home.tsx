5fbb0ae8a52f7de526b3fc20fb820c2b93f61e3d
krzysiek.krowka <krzysztof.krowka@onet.eu>
2024-09-28 18:41:13 +0200

    Exercise logic is finished

--- hackYeah/src/components/Exams/Home.tsx
+++ hackYeah/src/components/Exams/Home.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import Sidebar from '../Global/Sidebar';
-import { Exercise } from '../Exercises/Exercise';
-import { TestExercises } from '../Exercises/Exercise';
-import Exercises from '../Exercises/Exercises';
+import React from "react";
+import Sidebar from "../Global/Sidebar";
+import { Exercise } from "../Exercises/Exercise";
+import { TestExercises } from "../Exercises/Exercise";
+import Exercises from "../Exercises/Exercises";
 const ExamStart = () => {
-
-  return <>
-    {/* {TestExercises.forEach(exercise => <Exercises exercise={exercise}/>)} */}
-    {TestExercises.forEach(exercise => {return exercise.correctAnswer})}
-  </>;
+  return (
+    <>
+      <div className="flex bg-primary">
+        <Sidebar />
+        <div className="flex-1 m-4 rounded-sm p-2">
+          {TestExercises.map((exercise) => (
+            <Exercises exercise={exercise} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ExamStart;
