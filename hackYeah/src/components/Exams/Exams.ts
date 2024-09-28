96b02c90ed5c445a92d4ded21118fed998a3a267
Michał Kociołek <95742362+Michaluszeq@users.noreply.github.com>
2024-09-29 00:00:05 +0200

    s m

--- hackYeah/src/components/Exams/Exams.ts
+++ hackYeah/src/components/Exams/Exams.ts
@@ -1,26 +1,33 @@
 import { ExamType } from "../../type";
 import { FullExcercises } from "../Exercises/CoursesExcercises";
 
+// Tablica egzaminów, łącząca ćwiczenia z odpowiednich modułów
 export const Exams: ExamType[] = [
   {
     id: 0,
-    exercises: FullExcercises[0]
-      .concat(FullExcercises[1])
-      .concat(FullExcercises[2]),
+    exercises: [
+      ...FullExcercises[0],
+      ...FullExcercises[1],
+      ...FullExcercises[2],
+    ], // Łączenie ćwiczeń z modułów 1-3
     title: "Modules 1-3",
   },
   {
     id: 1,
-    exercises: FullExcercises[3]
-      .concat(FullExcercises[4])
-      .concat(FullExcercises[5]),
+    exercises: [
+      ...FullExcercises[3],
+      ...FullExcercises[4],
+      ...FullExcercises[5],
+    ], // Łączenie ćwiczeń z modułów 4-6
     title: "Modules 4-6",
   },
   {
     id: 2,
-    exercises: FullExcercises[6]
-      .concat(FullExcercises[7])
-      .concat(FullExcercises[8]),
+    exercises: [
+      ...FullExcercises[6],
+      ...FullExcercises[7],
+      ...FullExcercises[8],
+    ], // Łączenie ćwiczeń z modułów 7-9
     title: "Modules 7-9",
   },
 ];
