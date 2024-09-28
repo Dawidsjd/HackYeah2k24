b1f63fc6947d2869cc70b62d378f968e4ef23fe6
bezoski <rafalbuczkowski2004@wp.pl>
2024-09-28 15:46:59 +0200

    Update Card.tsx

--- hackYeah/src/components/Home/Card.tsx
+++ hackYeah/src/components/Home/Card.tsx
@@ -18,15 +18,15 @@ const Card: React.FC<CardProps> = ({
   buttonLink,
 }) => {
   return (
-    <div className="flex flex-col items-center text-center bg-gray-800 shadow-md rounded-lg p-4 w-1/3 ">
+    <div className="relative flex flex-col items-center text-center bg-gray-800 shadow-md rounded-lg p-4 w-1/3 h-64">
       <div className="bg-gray-700 rounded-full w-16 h-16 flex justify-center items-center">
         <p className="text-3xl text-gray-950">{step}</p>
       </div>
-      <p className="mt-1 text-xl bold text-gray-300">{title}</p>
-      <p className="text-gray-500">{description}</p>
+      <p className="mt-1 text-xl font-bold text-gray-300">{title}</p>
+      <p className="text-gray-500 flex-grow">{description}</p>
       <Link
         to={buttonLink}
-        className="bg-gray-900 text-gray-300 px-4 py-2 mt-2 mb-3 rounded-lg"
+        className="bg-gray-900 text-gray-300 px-4 py-2 rounded-lg absolute bottom-2  transform "
       >
         {buttonName}
       </Link>
