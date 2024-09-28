4baeae426e295f5d2fb0c484be044435c68ff08f
Zajenc <zajenc88@gmail.com>
2024-09-28 15:47:02 +0200

    typ kursu

--- hackYeah/src/components/Course/Course.tsx
+++ hackYeah/src/components/Course/Course.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Sidebar from '../Global/Sidebar';
 
+
+
 const Course = () => {
   return <Sidebar />;
+  
 };
 
 export default Course;
---
+++ hackYeah/src/components/Course/Courses.ts
@@ -0,0 +1,18 @@
+type CourseType = {
+  id: number;
+  title: String;
+  introduction: String;
+  mainArticlePartOne: String;
+  mainArticlePartTwo: String;
+  levelOfAdvancement: String;
+  linkToArticle: String;
+};
+let egg = {
+  id: 1,
+  title: "Kurs 1",
+  introduction: "Super kurs",
+  mainArticlePartOne: "LOrem ipsumdw oiim dwaiomwaio",
+  mainArticlePartTwo: "fawpkpkfpow opwk aop",
+  levelOfAdvancement: "begginer",
+  linkToArticle: "geg",
+};
