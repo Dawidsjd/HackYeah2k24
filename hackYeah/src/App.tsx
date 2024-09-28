79a32ccb88b5f9ac198d4050f9ff970a2f39e96d
Dawid Buszta <53567837+Dawidsjd@users.noreply.github.com>
2024-09-28 12:51:21 +0200

    tailwind init

--- hackYeah/src/App.tsx
+++ hackYeah/src/App.tsx
@@ -1,14 +1,12 @@
 
 import './App.css'
 
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-    <p>test</p>
-    </>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
