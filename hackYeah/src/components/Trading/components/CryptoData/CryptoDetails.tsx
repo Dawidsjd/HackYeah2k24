70f54ae00954daaf2cbd78e26d47b25e72b622b5
bezoski <rafalbuczkowski2004@wp.pl>
2024-09-29 06:35:45 +0200

    change colors in trading demo

--- hackYeah/src/components/Trading/components/CryptoData/CryptoDetails.tsx
+++ hackYeah/src/components/Trading/components/CryptoData/CryptoDetails.tsx
@@ -18,22 +18,28 @@ const CryptoDetails: React.FC<CryptoDetailsProps> = ({ onBack }) => {
   const [usdtLogo, setUsdtLogo] = useState('');
   const [btcPrice, setBtcPrice] = useState(0);
   const [alertVisible, setAlertVisible] = useState(false);
-  
+
   // Define the account balance as a state
   const [accountBalance, setAccountBalance] = useState(1000); // You can also fetch this dynamically if needed
 
   useEffect(() => {
     const fetchLogosAndRate = async () => {
       try {
-        const btcResponse = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
+        const btcResponse = await fetch(
+          'https://api.coingecko.com/api/v3/coins/bitcoin'
+        );
         const btcData = await btcResponse.json();
         setBtcLogo(btcData.image.small);
 
-        const usdtResponse = await fetch('https://api.coingecko.com/api/v3/coins/tether');
+        const usdtResponse = await fetch(
+          'https://api.coingecko.com/api/v3/coins/tether'
+        );
         const usdtData = await usdtResponse.json();
         setUsdtLogo(usdtData.image.small);
 
-        const priceResponse = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+        const priceResponse = await fetch(
+          'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+        );
         const priceData = await priceResponse.json();
         setBtcPrice(priceData.bitcoin.usd);
       } catch (error) {
@@ -124,40 +130,42 @@ const CryptoDetails: React.FC<CryptoDetailsProps> = ({ onBack }) => {
     <div className="relative p-4 ml-[30px]">
       <button
         onClick={onBack}
-        className="mb-4 text-blue-500 hover:underline"
+        className="mb-4 text-secondary hover:text-tertiary"
       >
         &lt; Back to Coins
       </button>
 
-      <Navbar/>
+      <Navbar />
 
       <div className="absolute transform scale-75 top-0 -right-5 w-64 overflow-hidden rounded-lg bg-gray-900 bg-opacity-30 backdrop-blur-md backdrop-filter border border-gray-800">
         <div className="absolute inset-0 bg-gradient-to-br from-gray-900 to-transparent opacity-50"></div>
         <div className="relative p-4">
-          <h2 className="text-lg font-semibold text-white mb-1">Account Balance</h2>
+          <h2 className="text-lg font-semibold text-white mb-1">
+            Account Balance
+          </h2>
           <div className="flex items-center justify-between">
-            <span className="text-2xl font-bold text-white">${accountBalance}</span>
-            <button 
-              className="text-white hover:bg-white hover:bg-opacity-10 p-1 rounded"
-            >
+            <span className="text-2xl font-bold text-white">
+              ${accountBalance}
+            </span>
+            <button className="text-white hover:bg-white hover:bg-opacity-10 p-1 rounded">
               <FiPlus className="h-5 w-5" />
               <span className="sr-only">Add funds</span>
             </button>
           </div>
         </div>
       </div>
-    
+
       <TradingViewWidget />
 
       <div className="flex justify-center space-x-2 mt-4">
-        <button 
+        <button
           onClick={() => handleOpenModal('buy')}
           className="flex-1 h-12 text-md font-semibold bg-green-500 hover:bg-green-600 text-white rounded-lg flex items-center justify-center space-x-2"
         >
           <ArrowUpCircle className="h-5 w-5" />
           <span>Buy</span>
         </button>
-        <button 
+        <button
           onClick={() => handleOpenModal('sell')}
           className="flex-1 h-12 text-md font-semibold bg-red-500 hover:bg-red-600 text-white rounded-lg flex items-center justify-center space-x-2"
         >
@@ -170,9 +178,13 @@ const CryptoDetails: React.FC<CryptoDetailsProps> = ({ onBack }) => {
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="modal modal-open">
             <div className="modal-box relative">
-              <h2 className="font-bold text-lg">{actionType === 'buy' ? 'Buy Confirmation' : 'Sell Confirmation'}</h2>
-              <button 
-                onClick={handleCloseModal} 
+              <h2 className="font-bold text-lg">
+                {actionType === 'buy'
+                  ? 'Buy Confirmation'
+                  : 'Sell Confirmation'}
+              </h2>
+              <button
+                onClick={handleCloseModal}
                 className="absolute top-2 right-4 text-gray-500 hover:text-gray-700 text-3xl"
               >
                 &times;
@@ -183,17 +195,21 @@ const CryptoDetails: React.FC<CryptoDetailsProps> = ({ onBack }) => {
                   <div className="py-4">
                     <label className="block text-sm">Amount in USD:</label>
                     <div className="flex items-center mb-2">
-                      <img src={usdtLogo} alt="USDT Logo" className="h-6 w-6 mr-2" />
+                      <img
+                        src={usdtLogo}
+                        alt="USDT Logo"
+                        className="h-6 w-6 mr-2"
+                      />
                     </div>
-                    <input 
-                      type="number" 
-                      value={usdAmount} 
+                    <input
+                      type="number"
+                      value={usdAmount}
                       onChange={(e) => handleUsdChange(e.target.value)}
                       className="input input-bordered w-full"
                       placeholder="Enter amount in USDT"
                     />
                   </div>
-                  
+
                   <div className="flex justify-center -mb-[40px] mt-[10px]">
                     <ArrowDownUp className="h-6 w-6 text-gray-500" />
                   </div>
@@ -201,17 +217,21 @@ const CryptoDetails: React.FC<CryptoDetailsProps> = ({ onBack }) => {
                   <div className="py-4">
                     <label className="block text-sm">Amount in BTC:</label>
                     <div className="flex items-center mb-2">
-                      <img src={btcLogo} alt="BTC Logo" className="h-6 w-6 mr-2" />
+                      <img
+                        src={btcLogo}
+                        alt="BTC Logo"
+                        className="h-6 w-6 mr-2"
+                      />
                     </div>
-                    <input 
+                    <input
                       type="text"
-                      value={btcAmount} 
+                      value={btcAmount}
                       onChange={(e) => handleBtcChange(e.target.value)}
                       className="input input-bordered w-full"
                       placeholder="Enter amount in BTC"
                     />
                   </div>
-                  <button 
+                  <button
                     onClick={handleExchange}
                     className="btn bg-amber-400 hover:bg-amber-300 w-full mt-4 text-black"
                   >
@@ -228,22 +248,24 @@ const CryptoDetails: React.FC<CryptoDetailsProps> = ({ onBack }) => {
       {alertVisible && (
         <motion.div
           role="alert"
-          initial={{ opacity: 0, x: 100 }}  // Initial state: hidden and off-screen to the right
-          animate={{ opacity: 1, x: 0 }}     // Animate to visible and move to original position
-          exit={{ opacity: 0, x: 100 }}       // Animate back to hidden and move off-screen to the right
-          transition={{ duration: 0.3 }}      // Transition duration
+          initial={{ opacity: 0, x: 100 }} // Initial state: hidden and off-screen to the right
+          animate={{ opacity: 1, x: 0 }} // Animate to visible and move to original position
+          exit={{ opacity: 0, x: 100 }} // Animate back to hidden and move off-screen to the right
+          transition={{ duration: 0.3 }} // Transition duration
           className="fixed bottom-4 right-4 w-1/2 z-50 alert alert-success mb-4 shadow-2xl"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6 shrink-0 stroke-current"
             fill="none"
-            viewBox="0 0 24 24">
+            viewBox="0 0 24 24"
+          >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
           </svg>
           <span>Your purchase has been confirmed!</span>
         </motion.div>
